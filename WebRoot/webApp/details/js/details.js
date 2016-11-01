67b6335fde738902dd1ae9a31e0fc35b6f1f0c06
iceBearIT <619667384@qq.com>
2016-11-01 16:38:30 +0800

    修改小bug

--- WebRoot/webApp/details/js/details.js
+++ WebRoot/webApp/details/js/details.js
@@ -127,7 +127,7 @@ function collect(){
 			success : function(data) {
 				   if(data != null && data != ""){
 					   	console.log(data);
-					   	switch("1"){
+					   	switch(data){
 					   	case "1":
 					   		document.getElementById("star").src = "img/collect.png";
 					   		alert("跟踪成功");
